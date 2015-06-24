b9c4e0049b42eca68ec8d6d81aba62f0e4999ed1
Andrés Mingorance <andres.mingorance@gmail.com>
2015-06-24 18:49:56 +0200

    chain.py goodies

--- utils/chain.py
+++ utils/chain.py
@@ -3,6 +3,16 @@ chain functions
 analogous to haskell's '.'
 '''
 
+# transform list parameter to tuple
+def accept_list(f):
+    def unpack(*args):
+        if isinstance(args[0], list):
+            args = args[0]
+        return f(*args)
+    return unpack
+
+
+@accept_list
 def chain(*methods):
     def concat(*args, **kw):
         res = methods[0](*args, **kw)
@@ -18,4 +28,5 @@ if __name__ == '__main__':
     add1 = lambda x : x + 1
     sub1 = lambda x : x - 1
 
-    print(chain(add1, add1, sub1)(1))
+    print( chain(add1, add1, sub1) (0) )
+    print( chain([add1, add1, sub1]) (0) )
