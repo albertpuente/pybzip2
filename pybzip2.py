280717b8dd0ddc51552db3e6724c8697d813d3ec
Andrés Mingorance <andres.mingorance@gmail.com>
2015-06-28 19:25:32 +0200

    removed prints from pybzip2

--- pybzip2.py
+++ pybzip2.py
@@ -20,97 +20,62 @@ class pybzip2compressor:
         self.content = None
 
     def compress(self):
-        print ("Start compress()")
-        print ("MSG: ", self.msg)
-        
         # Run-length encoding (RLE) of initial data
         res = rle_encode(self.msg)
-        print ("RLE: ", res)
-        
+
         # Burrows–Wheeler transform (BWT) or block sorting
         res, bwt_column = bwt_encode2(res)
         self.bwt_column = bwt_column
-        print ("BWT RES: ", res)
-        print ("BWT COL: ", bwt_column)
         symbols = list(set(res))
         # Move to front (MTF) transform
         res, _ = mtf_encode(res)
-        print ("MTF: ", res)
-        # self.mtf_stack = mtf_stack
-        
+
         # Run-length encoding (RLE) of MTF result
         res = rle2_encode(res)
         print ("RLE2: ", res)
         
         # Huffman coding
         coded_data, huffman_lengths, table_order = huffman_encode(res)
-        print("huffman lengths[0]:")
-        print("   ", huffman_lengths[0])
         self.content = bitChain(''.join(coded_data))
         self.huffman_groups = len(huffman_lengths)
-        print ("HUFFMAN GROUPS:", self.huffman_groups)
-        # print ("Content: ", self.content)
-        
+
         # Selection between multiple Huffman tables
         self.selectors_used = len(table_order)
-        print ("Selectors used:", self.selectors_used)
-        
+
         # store this to apply the mtf+unary encoding in packaging.py
         self.table_order = table_order
-        print ("Table order:", self.table_order)
 
         # Delta encoding (Δ) of Huffman code bit-lengths
         # self.delta_bit_length = []
         # for lengths in huffman_lengths:
         #    self.delta_bit_length += [delta_encode(lengths)]
         self.delta_bit_length = huffman_lengths
-        print ("Huf length:", self.delta_bit_length)
-        
+
         # Sparse bit array showing which symbols are used in our source
-        print ("Symbols to sparse:", symbols)
         self.bit_maps = sparse(symbols)
-        print ("Sparse:", self.bit_maps)
 
     def decompress(self):
         if self.content is None:
             raise Exception("Trying to decompress without having compressed")
-        print ("\nStart decompress()")
-        print ("Sparsed bit map:", self.bit_maps)
         # Sparse bit array showing which symbols are used
         symbols = unsparse(self.bit_maps)
         
         # remove the 0s
         huffman_symbols = list(range(1,258))
-        print(huffman_symbols)
-        print ("Sym (unsparse):", symbols)
-        
-        # Delta encoding (Δ) of Huffman code bit-lengths
-        # huffman_lengths = []
-        # for deltas in self.delta_bit_length:
-        #    huffman_lengths += [delta_decode(deltas)]
+
         huffman_lengths  = self.delta_bit_length
-        print ("Huf lengths", huffman_lengths)
-        
-        # Unary base 1 encoding of Huffman table selection
-        # Selection between multiple Huffman tables
+
         # Huffman coding
         res = self.content
-
-        # add runa, runb symbols
         res = huffman_decode(res, huffman_lengths,self.table_order, huffman_symbols)
-        # print ("HUF dec:", res)
         # Run-length encoding (RLE) of MTF result
         res = rle2_decode(res)
-        print ("RLE2 dec:", res)
         # Move to front (MTF) transform
         res = mtf_decode2(res, symbols)
-        print ("MTF dec:", res)
         # Burrows–Wheeler transform (BWT) or block sorting
         res = bwt_decode((res, self.bwt_column))
-        print ("BWT dec", res)
         # Run-length encoding (RLE) of initial data
         res = rle_decode(res)
-        print ("RLE DEC:", res)
         self.decompressed = res
 '''
 file_name = "Physics.cu"
